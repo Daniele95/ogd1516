e6373d191fd459532a2936454912b110078470be
Luke71017 <luca.uccellatori@gmail.com>
2016-05-04 15:30:56 +0100

    Added logic for projectile prefab. Needs to be polished.

---
+++ Assets/Scripts/ShootBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootBehaviour : MonoBehaviour {
+	private Rigidbody body;
+	public float speed = 50f;
+	public int hitPoints = 10;
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody> ();
+
+		body.velocity = transform.forward * speed;
+
+		Destroy (this.gameObject, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.CompareTag ("VehicleTeam0") && gameObject.CompareTag("BulletTeam1") || col.gameObject.CompareTag ("VehicleTeam1") && gameObject.CompareTag("BulletTeam0")) {
+			GuiVehicle gui = col.gameObject.GetComponent<GuiVehicle> ();
+			gui.life -= hitPoints;
+
+			Destroy (gameObject);
+		}
+	}
+}
