d6d270a353360b7dccb6a6e3958af8de62d62195
Luke71017 <luca.uccellatori@gmail.com>
2016-05-04 14:51:17 +0100

    Added basic camera script. It gives a few problems with rotation and near walls. Must be corrected before release.

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour 
+{
+	public Transform target;
+	public float distance = 10.0f;
+	public float smoothingValue = 5.0f;
+	public bool direct = true;
+
+	void Start()
+	{
+
+	}
+
+	void FixedUpdate()
+	{
+		if(target)
+		{
+			Vector3 targetPosition = target.position - transform.forward * distance;
+			if (direct)
+				transform.position = targetPosition;
+			else //smoothing
+				transform.position -= (transform.position - targetPosition) * smoothingValue * Time.deltaTime;
+		}
+	}
+}
