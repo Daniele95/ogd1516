1340ac66a85fda26430f1f56c5d925d1ab32d671
Luke71017 <luca.uccellatori@gmail.com>
2016-06-22 11:38:57 +0200

    Completed class choosing logic, need to bind it with the rest of the menu and to send what the player chose to the server.

--- Assets/ClassMenuManager.cs
+++ Assets/ClassMenuManager.cs
@@ -11,25 +11,46 @@ public class ClassMenuManager : MonoBehaviour {
     public Image arrowLeft;
     public Image health;
     public Image speed;
-    public Image hex_firstWeapon;       //hexes contains the logos
+    public Image hex_firstWeapon;       //hexes contains the logos and ammo information
     public Image hex_secondWeapon;
     public Image hex_specialAbility;
     public Text healthText;
     public Text speedText;
     public Text classText;
     public Text shortDescription;
+    public Text LblSpecialAbility;
 
     private GameObject canvas;
-    public GameObject activeMesh;
+    private GameObject activeMesh;
+    private string[] descriptions = new string[7];
+    private string[] classNames = new string[7];
+    private bool axisInUse;
+    private int currentConfiguration;
+    private const float AXIS_THRESHOLD = 0.3f;
+    private const int NUMBER_OF_CONFIGURATIONS = 7; 
 
     // Use this for initialization
     void Start () {
         canvas = gameObject;
-        drifterMesh.SetActive(true);
-        minerMesh.SetActive(false);
-        camperMesh.SetActive(false);
+        axisInUse = false;
+        currentConfiguration = 0;
+        ShowConfiguration(currentConfiguration);
 
-        activeMesh = drifterMesh;
+        classNames[0] = "Drifter";
+        classNames[1] = "Miner";
+        classNames[2] = "Camper";
+        classNames[3] = "Medical";
+        classNames[4] = "Rammer";
+        classNames[5] = "Chainer";
+        classNames[6] = "Defender";
+
+        descriptions[0] = "Short description of the Drifter class";
+        descriptions[1] = "Short description of the Miner class";
+        descriptions[2] = "Short description of the Camper class";
+        descriptions[3] = "Short description of the Medical class";
+        descriptions[4] = "Short description of the Rammer class";
+        descriptions[5] = "Short description of the Chainer class";
+        descriptions[6] = "Short description of the Defender class";
     }
 
     void FixedUpdate()
@@ -37,11 +58,108 @@ public class ClassMenuManager : MonoBehaviour {
         activeMesh.transform.Rotate(Vector3.up * 50 * Time.deltaTime);
     }
 
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            if (!axisInUse)
+            {
+                if (Input.GetAxisRaw("Horizontal") < -AXIS_THRESHOLD)
+                {
+                    previousConfiguration();
+                    //TODO: make leftArrow blink
+                }
+                else if (Input.GetAxisRaw("Horizontal") > AXIS_THRESHOLD)
+                {
+                    nextConfiguration();
+                    //TODO: make rightArrow blink
+                }
+                axisInUse = true;
+            }
+        }
+        else
+            axisInUse = false;
+    }
+
+    private void nextConfiguration()
+    {
+        ShowConfiguration((++currentConfiguration) % NUMBER_OF_CONFIGURATIONS);
+    }
+
+    private void previousConfiguration()
+    {
+        ShowConfiguration((--currentConfiguration) % NUMBER_OF_CONFIGURATIONS);
+    }
+
+    private void ShowConfiguration(int configuration)
+    {
+        if (configuration > (NUMBER_OF_CONFIGURATIONS - 1) || configuration < 0)
+            configuration = 0;
+
+        switch(configuration)
+        {
+            case 0:         //Drifter
+                activateDrifter();
+                //changeweaponlogo
+                hex_firstWeapon.GetComponentInChildren<Text>().text = "125";
+                hex_secondWeapon.GetComponentInChildren<Text>().text = "-";
+                hex_specialAbility.gameObject.SetActive(true);
+                LblSpecialAbility.gameObject.SetActive(true);
+                //specialabilitylogo
+                break;
+            case 1:         //Miner
+                activateMiner();
+                hex_firstWeapon.GetComponentInChildren<Text>().text = "10";
+                hex_secondWeapon.GetComponentInChildren<Text>().text = "12";
+                hex_specialAbility.gameObject.SetActive(false);
+                LblSpecialAbility.gameObject.SetActive(false);
+                break;
+            case 2:         //Camper
+                activateCamper();
+                hex_firstWeapon.GetComponentInChildren<Text>().text = "8";
+                hex_secondWeapon.GetComponentInChildren<Text>().text = "10";
+                hex_specialAbility.gameObject.SetActive(true);
+                LblSpecialAbility.gameObject.SetActive(true);
+                break;
+            case 3:         //Medical
+                activateFake();
+                hex_firstWeapon.GetComponentInChildren<Text>().text = "90";
+                hex_secondWeapon.GetComponentInChildren<Text>().text = "10";
+                hex_specialAbility.gameObject.SetActive(true);
+                LblSpecialAbility.gameObject.SetActive(true);
+                break;
+            case 4:         //Rammer
+                activateFake();
+                hex_firstWeapon.GetComponentInChildren<Text>().text = "-";
+                hex_secondWeapon.GetComponentInChildren<Text>().text = "90";
+                hex_specialAbility.gameObject.SetActive(false);
+                LblSpecialAbility.gameObject.SetActive(false);
+                break;
+            case 5:         //Chainer
+                activateFake();
+                hex_firstWeapon.GetComponentInChildren<Text>().text = "-";
+                hex_secondWeapon.GetComponentInChildren<Text>().text = "-";
+                hex_specialAbility.gameObject.SetActive(false);
+                LblSpecialAbility.gameObject.SetActive(false);
+                break;
+            case 6:         //Defender
+                activateFake();
+                hex_firstWeapon.GetComponentInChildren<Text>().text = "-";
+                hex_secondWeapon.GetComponentInChildren<Text>().text = "12";
+                hex_specialAbility.gameObject.SetActive(false);
+                LblSpecialAbility.gameObject.SetActive(false);
+                break;
+        }
+        shortDescription.text = descriptions[configuration];
+        classText.text = classNames[configuration];
+    }
+
     private void activateDrifter()
     {
         drifterMesh.SetActive(true);
         minerMesh.SetActive(false);
         camperMesh.SetActive(false);
+        //fakeMesh.SetActive(false);
 
         activeMesh = drifterMesh;
     }
@@ -51,6 +169,7 @@ public class ClassMenuManager : MonoBehaviour {
         drifterMesh.SetActive(false);
         minerMesh.SetActive(true);
         camperMesh.SetActive(false);
+        //fakeMesh.SetActive(false);
 
         activeMesh = minerMesh;
     }
@@ -60,7 +179,17 @@ public class ClassMenuManager : MonoBehaviour {
         drifterMesh.SetActive(false);
         minerMesh.SetActive(false);
         camperMesh.SetActive(true);
+        //fakeMesh.SetActive(false);
 
         activeMesh = camperMesh;
+    }
+
+    private void activateFake()
+    {
+        drifterMesh.SetActive(false);
+        minerMesh.SetActive(false);
+        camperMesh.SetActive(false);
+
+        //fakeMesh.SetActive(true);
     }
 }
