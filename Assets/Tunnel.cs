d5284bd674c082b973061c45863261b23fcf1d50
☆ M.Elena Silletti < M.Elena Silletti>
2016-06-24 14:15:17 +0200

    Implemented motion blur inside tunnels

--- Assets/Tunnel.cs
+++ Assets/Tunnel.cs
@@ -1,41 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Tunnel : MonoBehaviour {
-	public bool reverse;
+namespace UnityStandardAssets.ImageEffects
+{
+    public class Tunnel : MonoBehaviour
+    {
+        public bool reverse;
 
-	public GameObject listPoints;
+        public GameObject listPoints;
 
-	void Start () {
-	
-	}
-	
-	void Update () {
-	
-	}
+        void Start()
+        {
 
-	void OnTriggerEnter(Collider other){
-		if (other.gameObject.CompareTag ("VehicleTeam0") || other.gameObject.CompareTag ("VehicleTeam1")) {
-			SimpleController scriptController = other.gameObject.GetComponent<SimpleController> ();
+        }
 
-			if (scriptController.inTunnel == 0) {
-				scriptController.inTunnel = 1;
-				scriptController.targetWayPoint = null;
-				scriptController.reverseTunnel = reverse;
-				scriptController.wayPointList = listPoints;
-			}
-		}
-	}
+        void Update()
+        {
 
-	void OnTriggerExit(Collider other){
-		/*print (other.gameObject.name + " uscito");
+        }
 
-		if (other.gameObject.CompareTag ("VehicleTeam0") || other.gameObject.CompareTag ("VehicleTeam1")) {
-			Tunnel tunnelScript = endTunnel.GetComponent<Tunnel>();
-			if(tunnelScript.
-			SimpleController scriptController = other.gameObject.GetComponent<SimpleController> ();
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("VehicleTeam0") || other.gameObject.CompareTag("VehicleTeam1"))
+            {
+                SimpleController scriptController = other.gameObject.GetComponent<SimpleController>();
 
-			scriptController.inTunnel = 0;
-		}*/
-	}
+                if (scriptController.inTunnel == 0)
+                {
+                    scriptController.inTunnel = 1;
+                    GameObject.Find("MainCamera").GetComponent<MotionBlur>().enabled = true;
+                    scriptController.targetWayPoint = null;
+                    scriptController.reverseTunnel = reverse;
+                    scriptController.wayPointList = listPoints;
+                }
+
+            }
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.CompareTag("VehicleTeam0") || other.gameObject.CompareTag("VehicleTeam1"))
+            {
+                SimpleController scriptController = other.gameObject.GetComponent<SimpleController>();
+                if (scriptController.currentWayPoint <= 0 ||scriptController.currentWayPoint>14)
+                {
+                    GameObject.Find("MainCamera").GetComponent<MotionBlur>().enabled = false;
+                }
+                /*print (other.gameObject.name + " uscito");
+
+                if (other.gameObject.CompareTag ("VehicleTeam0") || other.gameObject.CompareTag ("VehicleTeam1")) {
+                    Tunnel tunnelScript = endTunnel.GetComponent<Tunnel>();
+                    if(tunnelScript.                     || scriptController.currentWayPoint < scriptController.wayPointList.transform.childCount
+                    SimpleController scriptController = other.gameObject.GetComponent<SimpleController> ();
+                    scriptController.inTunnel = 0;*/
+
+            }
+        }
+    }
 }
