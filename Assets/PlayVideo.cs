f821970724c1a104b884f00b588e4c9bfa64f456
☆ M.Elena Silletti < M.Elena Silletti>
2016-06-28 09:42:49 +0200

    Intro to the game

---
+++ Assets/PlayVideo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayVideo : MonoBehaviour {
+    public MovieTexture movTexture;
+
+	// Use this for initialization
+	void Start () {
+	    GetComponent<Renderer>().material.mainTexture = movTexture;
+        movTexture.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!movTexture.isPlaying)
+        {
+            SceneManager.LoadScene("Main menu");
+        }
+	}
+}
